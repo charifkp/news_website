aae929379b4cc63f6b05a476bcae0dd04514c842
Muhammad Charif <muhahmadcharif74@gmail.com>
2026-05-31 16:08:37 +0700

    Upload project

---
+++ implements/backend/src/index.js
@@ -0,0 +1,72 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+
+const FRONTEND_SRC = path.join(__dirname, '..', '..', 'frontend', 'src');
+const LOCAL_PAGES = ['/index.html','/upload.html','/dashboard.html','/news-admin.html'];
+
+
+const mimeTypes = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'text/javascript',
+  '.webp': 'image/webp', // Now this will actually be used!
+  '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.png': 'image/png',
+  '.svg':  'image/svg+xml',
+  '.ico':  'image/x-icon',
+  '.pdf':  'application/pdf',
+};
+
+
+
+
+
+const server = http.createServer((req, res) => {
+  const urlPath = req.url.split('?')[0];
+  // 1. DYNAMIC PATH: If request is "/" use "/index.html", otherwise use what the browser asked for (like "/image/gradualfact.webp")
+  const reqUrl = urlPath === '/' ? '/index.html' : urlPath;
+  
+  // 2. Combine it dynamically so it finds the correct file
+  // const filepath = path.join(__dirname, '..', '..', 'frontend', 'src', reqUrl);
+  let filepath = path.join(FRONTEND_SRC, reqUrl);
+  
+
+
+
+  const extname = String(path.extname(filepath)).toLowerCase();
+
+  // const mimeTypes = {
+  //   '.html': 'text/html',
+  //   '.css': 'text/css',
+  //   '.js': 'text/javascript',
+  //   '.webp': 'image/webp', // Now this will actually be used!
+  //   '.jpg': 'image/jpeg',
+  //   '.png': 'image/png'
+  // };
+
+  const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filepath, (err, data) => {
+    if (err) {
+      // console.error(`404: ${filepath}`);
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('404 Not Found');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(data);
+    }
+  });
+});
+
+server.listen(3002, '0.0.0.0', () => {
+  console.log('Server running at http://127.0.0.1:3002');
+  console.log('Backend is running');
+
+  console.log('/                 --> index.html (mainpage)');
+  console.log('/upload           --> upload.html (upload)');
+  console.log('/admin-login.html --> admin-login.html (admin-login)');
+  console.log('/dashboard        --> dashboard.html (dashboard)');
+});
